70f04e8f70a13605143ba33ae77745e8d8f54348
Trần Phương Nam <nam.job96@gmail.com>
2023-10-08 15:13:38 +0700

    - config client port

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,18 @@
 import { fileURLToPath, URL } from 'url'
-
+import dotenv from "dotenv";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+dotenv.config();
+
+const PORT = parseInt(process.env.VITE_APP_PORT) || 3000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: PORT,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
